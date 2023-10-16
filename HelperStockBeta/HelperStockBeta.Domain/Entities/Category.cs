1cb31e2835c2a68fc627849c8d0a0187a09c635e
liviafioraneli <liviafioraneli@hotmail.com>
2023-10-16 12:29:12 -0300

    Aula 16/10/2023

---
+++ HelperStockBeta/HelperStockBeta.Domain/Entities/Category.cs
@@ -0,0 +1,38 @@
+﻿using HelperStockBeta.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+
+        //Relações de entidades
+        public ICollection<Product> Products { get; set; }
+        public Category(string name)
+        {
+            ValidateDomain(name);
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Identification is positive values");
+            Id = id;
+            ValidateDomain(name);
+            Name = name;
+        }
+        public void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length > 3, "Name is minimum 3 characters");
+        }
+    }
+}
