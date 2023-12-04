0711dac7142bd9f344067905f948943dc36d7661
liviafioraneli <liviafioraneli@hotmail.com>
2023-12-04 12:01:03 -0300

    Aula 0412 Swagger e camada de API, referência para prova

---
+++ HelperStockBeta/HelperStockBeta.Application/Interface/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using HelperStockBeta.Application.DTOs;
+
+namespace HelperStockBeta.Application.Interface
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDTO);
+        Task Update(CategoryDTO categoryDTO);
+        Task Remove(int? id);
+
+    }
+}
---
+++ HelperStockBeta/HelperStockBeta.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HelperStockBeta.Application.DTOs;
+using HelperStockBeta.Domain.Entities;
+
+namespace HelperStockBeta.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+      public DomainToDTOMappingProfile() 
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+
+    }
+}
